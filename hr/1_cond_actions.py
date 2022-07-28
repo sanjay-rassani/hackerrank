32f82f677c903e32f099a9de88c0487a6a3e45fb
Sanjay Kumar <sanjay@bitswits.com>
2022-07-28 13:28:34 +0500

    1 conitional actions

---
+++ hr/1_cond_actions.py
@@ -0,0 +1,21 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+
+n = int(input('Enter a number: '))
+
+if n % 2 != 0:
+    print('Weird')
+else:
+    if n >= 2 and n <= 5:
+        print('Not Weird')
+    elif n >= 6 and n <= 20:
+        print('Weird')
+    elif n > 20:
+        print('Not Weird')
+        
